0245edf631c45013fbd4b4fb9e6b8138e8190632
Antuh <antuhsergey648@gmail.com>
2023-10-31 18:41:08 +0700

    Initial ten commit

--- app/src/main/java/com/example/dora/EndCharacterActivity.kt
+++ app/src/main/java/com/example/dora/EndCharacterActivity.kt
@@ -1,44 +1,63 @@
 package com.example.dora
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class EndCharacterActivity : AppCompatActivity() {
+    private val PREF_NAME = "MyPrefs"
+    private val PERCENT_KEY = "percent"
+
+    var handler: Handler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_character)
 
-
+        val sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        var percent = sharedPreferences.getInt(PERCENT_KEY, 0)
         val imageByteArray = intent.getByteArrayExtra("imageByteArray")
-
         if (imageByteArray != null) {
-            val selectedImage =
-                BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.size)
+            val selectedImage = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.size)
 
             val imageView = findViewById<ImageView>(R.id.your_image_view_id)
             imageView.setImageBitmap(selectedImage)
         }
 
-        var handler = Handler()
-        var percent = 0
-        val  updateVariable = object : Runnable {
+        handler = Handler()
+        val updateVariable = object : Runnable {
             override fun run() {
                 percent++
                 val textView: TextView = findViewById(R.id.textViewThree)
                 textView.text = "Создание персонажа $percent %"
-                handler.postDelayed(this,50)
-                if (percent == 95){
+                handler?.postDelayed(this, 50)
+                if (percent == 100) {
+                    handler?.removeCallbacks(this)
+                    with(sharedPreferences.edit()) {
+                        putInt(PERCENT_KEY, percent)
+                        apply()
+                    }
                     startActivity(Intent(this@EndCharacterActivity, ChatCharacterActivity::class.java))
                     finish()
-
+                } else {
+                    with(sharedPreferences.edit()) {
+                        putInt(PERCENT_KEY, percent)
+                        apply()
+                    }
                 }
             }
         }
-        handler.postDelayed(updateVariable,50)
+        handler?.postDelayed(updateVariable, 50)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler?.removeCallbacksAndMessages(null)
     }
 }
--- app/src/main/java/com/example/dora/StepOneActivity.kt
+++ app/src/main/java/com/example/dora/StepOneActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dora
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,8 @@ class StepOneActivity : AppCompatActivity() {
     private lateinit var textInput: EditText
     private lateinit var textNext: TextView
     private lateinit var sharedPreferences: SharedPreferences
+    private val PREF_NAME = "MyPrefs"
+    private val PERCENT_KEY = "percent"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,44 +23,53 @@ class StepOneActivity : AppCompatActivity() {
         textNext = findViewById(R.id.textViewThree)
         sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE)
 
-        val enteredText = sharedPreferences.getString("enteredText", null)
-        val savedName = sharedPreferences.getString("characterName", null)
+        val sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val percent = sharedPreferences.getInt(PERCENT_KEY, 0)
 
-        //val editor = sharedPreferences.edit()
-        //editor.remove("enteredText")
-        //editor.apply()
-
-        if (savedName != null) {
-            val intent = Intent(this, PurposesActivity::class.java)
-            intent.putExtra("characterName", savedName)
-            startActivity(intent)
-        }
-        if (enteredText != null) {
-            val intent = Intent(this, StepTwoActivity::class.java)
-            intent.putExtra("textToPass", enteredText)
-            startActivity(intent)
-            finish()
+        if (percent == 100) {
+            startActivity(Intent(this, ChatCharacterActivity::class.java))
         } else {
-            textNext.setOnClickListener {
-                onButtonClick(textInput)
+            val enteredText = sharedPreferences.getString("enteredText", null)
+            val savedName = sharedPreferences.getString("characterName", null)
+
+            //val editor = sharedPreferences.edit()
+            //editor.remove("enteredText")
+            //editor.apply()
+
+            if (savedName != null) {
+                val intent = Intent(this, PurposesActivity::class.java)
+                intent.putExtra("characterName", savedName)
+                startActivity(intent)
+            }
+            if (enteredText != null) {
+                val intent = Intent(this, StepTwoActivity::class.java)
+                intent.putExtra("textToPass", enteredText)
+                startActivity(intent)
+                finish()
+            } else {
+                textNext.setOnClickListener {
+                    onButtonClick(textInput)
+                }
+            }
+        }
+    }
+        fun onButtonClick(view: View) {
+            val enteredText = textInput.text.toString().trim()
+
+            if (enteredText.isNotEmpty()) {
+                val editor = sharedPreferences.edit()
+                editor.putString("enteredText", enteredText)
+                editor.apply()
+
+                val intent = Intent(this, StepTwoActivity::class.java)
+                intent.putExtra("textToPass", enteredText)
+                startActivity(intent)
+                finish()
+            } else {
+                textInput.error = "Введите имя"
             }
         }
     }
 
-    fun onButtonClick(view: View) {
-        val enteredText = textInput.text.toString().trim()
 
-        if (enteredText.isNotEmpty()) {
-            val editor = sharedPreferences.edit()
-            editor.putString("enteredText", enteredText)
-            editor.apply()
 
-            val intent = Intent(this, StepTwoActivity::class.java)
-            intent.putExtra("textToPass", enteredText)
-            startActivity(intent)
-            finish()
-        } else {
-            textInput.error = "Введите имя"
-        }
-    }
-}
